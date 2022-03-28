3b27267583e22de80bd2362451ebdaf89232ed44
Chanyoung Lee <cyl0504@gmail.com>
2022-03-28 23:36:13 +0900

    day_13

--- 16. 시저암호/answer.py
+++ 16. 시저암호/answer.py
@@ -1,8 +1,24 @@
-def solution(n):
-    answer = 0
-    for i in range(1,n+1):
-        if n % i == 0:
-            answer += i
-    return answer
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
 
+    return "".join(s)
+    
+# 실패한 코드
+# def solution(s, n):
+#     answer = ''
+#     s_list = list(s)
+#     blank_list = []
+#     for i in range(len(s_list)):
+#         if s_list[i] == ' ':
+#             blank_list.append(s_list[i])
+#         else:
+#             blank_list.append(ord(s_list[i]))
+#     for x in range(len(s_lsit)):
+#         if s_list
+#     return answer
 print(solution(12))
