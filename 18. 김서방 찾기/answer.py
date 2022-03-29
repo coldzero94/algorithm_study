e50cb5cd0d2f3fdb9b8ac63cba63eda2b15d43f2
Chanyoung Lee <cyl0504@gmail.com>
2022-03-29 16:50:11 +0900

    day_14_2

--- 18. 김서방 찾기/answer.py
+++ 18. 김서방 찾기/answer.py
@@ -1,30 +1,6 @@
-
-import math
-
-def solution(number):
-    numbers = [True] * (number + 1)
-    answer = 0
-
-    for num in range(2,int(math.sqrt(number))+1):
-        if numbers[num] == False:
-            continue;
-
-        for i in range(num+num,number+1,num):
-            numbers[i] = False
-
-    for i in range(2,number+1):
-        if numbers[i] == True:
-            answer+=1
-
-    return answer
-    # 아래는 틀린 
-# def solution(n):
-#     answer = 0
-#     blank_list = []
-#     for i in range(1,n+1):
-#         if i==2 or i==3 or i==5 or i==7:
-#             blank_list.append(i)
-#         elif i%i == 0 and i!=1 and i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
-#             blank_list.append(i)
-#     answer = len(blank_list)
-#     return answer
+def solution(seoul):
+    answer1 = '김서방은'
+    answer2 = '에 있다'
+    find_kim = 'Kim'
+    last_name = [i for i in range(len(seoul)) if find_kim in seoul[i]]
+    return answer1+" "+str(last_name[0])+answer2
--- 18. 김서방 찾기/test.md
+++ 18. 김서방 찾기/test.md
@@ -1,24 +1,20 @@
 
    
-# 약수의 합
+# 김서방 찾기
 
 ## 문제 설명
-1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
-
-소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
-(1은 소수가 아닙니다.)
+String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
 
 ## 제한 조건
 
-- n은 2이상 1000000이하의 자연수입니다.
-
+- seoul은 길이 1 이상, 1000 이하인 배열입니다.
+- seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+- "Kim"은 반드시 seoul 안에 포함되어 있습니다.
 ## 입출력 예
 
-|s|n|result|
-|----|----|----|
-|"AB"|1|"BC"|
-|"z"|1|"a"|
-|"a B z"|4|"e F d"|
+|seoul|return|
+|----|----|
+|"Jane","Kim"|"김서방은 1에 있다"|
 ----
 
 ## 문제 접근 로그
@@ -31,4 +27,4 @@
 2. math, sqrt
 
 ## 문제 url
-https://programmers.co.kr/learn/courses/30/lessons/12921
+https://programmers.co.kr/learn/courses/30/lessons/12919
