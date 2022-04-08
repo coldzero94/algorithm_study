27c04fe1e7ef61b005f098b6f2f96422ef4abf27
Chanyoung Lee <cyl0504@gmail.com>
2022-04-08 18:33:31 +0900

    day_20

---
+++ 26. 같은 숫자는 싫어/answer.py
@@ -0,0 +1,18 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12906
+
+def solution(arr):
+    answer = []
+    check_num = len(arr)-2
+    last_num = len(arr)-1
+    for i in range(len(arr)):
+        if i == (len(arr)-1):
+            break
+        if arr[i] != arr[i+1]:
+            answer.append(arr[i])
+    if arr[check_num] != arr[last_num]:
+        answer.append(arr[last_num])
+    if arr[check_num-1] != arr[check_num]:
+        answer.append(arr[check_num])
+    return answer
+
+print(solution([1,1,1,5,5,1,1,9]))
