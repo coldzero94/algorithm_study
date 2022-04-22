78ed680dd12e4fa314af3d5dd58a882060bef04f
Chanyoung Lee <cyl0504@gmail.com>
2022-04-22 21:30:43 +0900

    day_29

---
+++ 35. 예산/answer.py
@@ -0,0 +1,10 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12982
+def solution(d, budget):
+    answer = 0
+    for i in sorted(d):
+        budget -= i
+        if budget < 0:
+            break
+        answer += 1
+        
+    return answer
