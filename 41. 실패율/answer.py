51f80369e6949aa6322da42b322df035d1a91e78
Chanyoung Lee <cyl0504@gmail.com>
2022-05-08 21:00:00 +0900

    day_34

---
+++ 41. 실패율/answer.py
@@ -0,0 +1,13 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42889
+
+def solution(N, stages):
+    People = len(stages)
+    faillist = {}
+    for i in range(1, N + 1):
+        if People != 0:
+            faillist[i] = stages.count(i) / People
+            People -= stages.count(i)
+        else:
+            faillist[i] = 0
+
+    return sorted(faillist, key=lambda i: faillist[i], reverse=True)
